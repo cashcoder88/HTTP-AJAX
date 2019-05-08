053ecbfbc50e856fdce84581f67a93fec64eecaa
cashcoder88 <cashglobe@gmail.com>
2019-05-08 14:39:14 -0600

    Created comp to display list of friends, did axios request succesfully

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,12 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Friends from './Components/Friends';
+import axios from "axios";
+
+
+
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+    .get("http://localhost:5000/friends")
+    .then(res => {
+      console.log(res);
+      this.setState( {friends: res.data} )
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <Friends />
+        <Friends 
+          friends={this.state.friends}
+        />
       </div>
     );
   }
--- friends/src/Components/Friends.js
+++ friends/src/Components/Friends.js
@@ -3,36 +3,16 @@ import React from 'react';
 const Friends = (props) => {
     return (
         <div className='friends-container'>
-            {props.array.map((friend) => {
+            {props.friends.map((friend) => (
                 <div className='friends' key={friend.id}>
                     <h1>Name: {friend.name}</h1>
                     <h3>Age: {friend.age}</h3>
                     <h3>Email Address: {friend.email}</h3>
+                    <h1>Testing</h1>
                 </div>
-            })}
+            ))}
         </div>
     )
 }
 
 export default Friends;
-
-// import React from 'react';
-// import './StarWars.css';
-
-// const Chars = (props) => {
-//     return (
-//     <div className="containerDiv">
-//         {props.array.map((char) => (
-//           <div className="character" key={char.url}>
-//             <h1 className="name">{char.name}</h1>
-//             <p className="height">Height: {char.height}cm</p>
-//             <p className="weight">Weight: {char.mass}kg</p>
-//             <p className="birth_year">Birth Year: {char.birth_year}</p>
-//           </div>
-//         ))}
-//     </div>
-//     )
-// }
-
-
-// export default Chars;
